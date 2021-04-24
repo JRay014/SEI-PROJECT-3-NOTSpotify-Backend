ea2afec513a98f7277318998b69bcfb6849d2af3
John Raymond <jray014@gmail.com>
2021-04-24 00:48:32 -0400

    dotenv i

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const session = require('express-session')
+require('dotenv').config()
 
 const mongoose = require('mongoose');
 const cors = require('cors')
